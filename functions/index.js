83fd979cf57ebd40f5361496a70fc5ffb86d20f5
Paul Jones <pjanvil@gmail.com>
2019-02-11 17:05:01 +0000

    initial

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+const createUser  = require('./create_user');
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+
+exports.goodbyt = functions.https.onRequest((request, response) => {
+  response.send("Bye");
+});
+
+exports.createUser = functions.https.onRequest(createUser);
